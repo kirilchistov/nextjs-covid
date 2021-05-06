29f5d6f4cbe048cd7d55eccad8e26c1184bb6100
kirilchistov <the.freedom.of.essence@gmail.com>
2021-05-06 15:05:07 +0300

    create repository

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+const Header = (props) => {
+  return (
+    <header className="text-center mt-0">
+      <div className="bg-gray-800 shadow-md ">
+        <h1 className="p-3 text-xl font-semibold text-gray-200">COVID 19</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
