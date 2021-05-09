d93e1c38ec576ec57ad2093cd368cc312aea7772
kirilchistov <the.freedom.of.essence@gmail.com>
2021-05-09 14:36:15 +0300

    i will continue when i find a good api

---
+++ pages/country/index.tsx
@@ -0,0 +1,13 @@
+import { useRef } from 'react';
+import Header from '../../components/Header';
+
+const country = () => {
+  return (
+    <div>
+      <Header />
+      Country page
+    </div>
+  );
+};
+
+export default country;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,25 @@
-import axios from 'axios';
-import Head from 'next/head';
 import { useEffect, useState } from 'react';
 import Header from '../components/Header';
-import moment from 'moment';
 
 export default function Home() {
   const [data, setData] = useState([]);
-  const [filter, setFilter] = useState('');
 
   useEffect(async () => {
-    try {
-      const res = await axios('https://api.covid19api.com/summary ');
-      setData(res.data.Countries);
-    } catch (err) {
-      console.log(err);
-    }
-  }, []);
+    await fetch('https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php', {
+      headers: {
+        'x-rapidapi-key': '7887d314femsh169290428000428p199f93jsn9470329ba9cb',
+        'x-rapidapi-host': 'coronavirus-monitor.p.rapidapi.com',
+      },
+    })
+      .then((res) => console.log(res.json()))
+      // .catch(err = console.log(err));
+  });
+  // console.log(data);
 
   return (
     <div>
       <Header />
-      <div className="container mx-auto text-yellow-50 text-center">
-      <input className="text-gray-900 p-1 mt-3" type="text" onChange={(e) => setFilter(e.target.value)} />
-        {data
-          .filter((val) => {
-            if (filter === '') {
-              return val;
-            } else if (val.Country.toLowerCase().includes(filter.toLocaleLowerCase())) {
-              return val;
-            }
-          })
-          .map((val, index) => {
-            return (
-              <div className="text-gray-300 font-medium" key={index}>
-                <br />
-                <p>Country: {val.Country}</p>
-                <p>
-                  Total Confirmed: <span className="text-blue-600">{val.TotalConfirmed}</span>
-                </p>
-                <p>
-                  Total Death: <span className="text-red-500">{val.TotalDeaths}</span>
-                </p>
-                <p>
-                  Total Recovered: <span className="text-green-500">{val.TotalRecovered}</span>
-                </p>
-              </div>
-            );
-          })}
-      </div>
+      <div className="container mx-auto text-yellow-50 text-center"></div>
     </div>
   );
 }
